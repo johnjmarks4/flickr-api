3f768f50861a613bda785eb6fa2f9f42e0a92bf6
John Marks <johnjmarks4@gmail.com>
2018-06-15 22:58:43 -0500

    Make images into links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
     if !params[:id].nil?
       list = flickr.photos.search(:user_id => params[:id])
       list.each do |pic|
-        @links << URI.parse("https://www.flickr.com/photos/#{pic["owner"]}/#{pic["id"]}")        
+        @links << "https://www.flickr.com/photos/#{pic["owner"]}/#{pic["id"]}"     
         @pictures << URI.parse("http://farm#{pic["farm"]}.staticflickr.com/#{pic["server"]}/#{pic["id"]}_#{pic["secret"]}.jpg")
       end
     end
