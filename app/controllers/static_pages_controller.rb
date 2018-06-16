180f009ce4a0d9734afe3b75e28fe1b65450c614
John Marks <johnjmarks4@gmail.com>
2018-06-15 22:45:28 -0500

    Make images clickable

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,11 @@ require 'flickraw'
 class StaticPagesController < ApplicationController
   def home
     @pictures = []
+    @links = []
     if !params[:id].nil?
       list = flickr.photos.search(:user_id => params[:id])
       list.each do |pic|
+        @links << URI.parse("https://www.flickr.com/photos/#{pic["owner"]}/#{pic["id"]}")        
         @pictures << URI.parse("http://farm#{pic["farm"]}.staticflickr.com/#{pic["server"]}/#{pic["id"]}_#{pic["secret"]}.jpg")
       end
     end
