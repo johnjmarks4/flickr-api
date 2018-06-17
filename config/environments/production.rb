3f3f0a1f221bd10dd9f4ac4dbbde083920e18b7f
John Marks <johnjmarks4@gmail.com>
2018-06-16 23:03:11 -0500

    Add background image to top pane

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
