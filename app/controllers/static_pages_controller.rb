00266f33ae0abc61be2d686557abc65abd439563
John Marks <johnjmarks4@gmail.com>
2018-06-21 18:21:43 -0500

    added flash

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,13 +4,17 @@ class StaticPagesController < ApplicationController
   def home
     @pictures = []
     @links = []
-    if !params[:id].nil?
-      list = flickr.photos.search(:user_id => params[:id])
-      list.each do |pic|
-        @links << "https://www.flickr.com/photos/#{pic["owner"]}/#{pic["id"]}"     
-        @pictures << URI.parse("http://farm#{pic["farm"]}.staticflickr.com/#{pic["server"]}/#{pic["id"]}_#{pic["secret"]}.jpg")
+    begin
+      if !params[:id].nil?
+        list = flickr.photos.search(:user_id => params[:id])
+        list.each do |pic|
+          @links << "https://www.flickr.com/photos/#{pic["owner"]}/#{pic["id"]}"     
+          @pictures << URI.parse("http://farm#{pic["farm"]}.staticflickr.com/#{pic["server"]}/#{pic["id"]}_#{pic["secret"]}.jpg")
+        end
       end
+    rescue
+      flash[:alert] = "User not found"
+      redirect_to root_path
     end
-    render "static_pages/home"
   end
 end
